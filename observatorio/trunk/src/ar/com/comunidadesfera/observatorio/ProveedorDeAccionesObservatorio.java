96c3bfc86cd7a6537c44f2f24959f2cf9e331e4f
marianotugnarelli@gmail.com <marianotugnarelli@gmail.com@8acefad2-8f69-11de-b0c3-d3322c7aa8e4>
2014-07-29 20:28:38 +0000

    Seperación de las clases extendidas de JDT a un fragmento.

--- observatorio/trunk/src/ar/com/comunidadesfera/observatorio/ProveedorDeAccionesObservatorio.java
+++ observatorio/trunk/src/ar/com/comunidadesfera/observatorio/ProveedorDeAccionesObservatorio.java
@@ -1,19 +1,11 @@
 package ar.com.comunidadesfera.observatorio;
 
-import org.eclipse.jdt.internal.ui.actions.OpenTypeAction;
-import org.eclipse.jdt.internal.ui.wizards.buildpaths.newsourcepage.GenerateBuildPathActionGroup;
-import org.eclipse.jdt.ui.actions.CCPActionGroup;
-import org.eclipse.jdt.ui.actions.GenerateActionGroup;
-import org.eclipse.jdt.ui.actions.JavaSearchActionGroup;
-import org.eclipse.jdt.ui.actions.OpenProjectAction;
 import org.eclipse.jdt.ui.actions.OpenViewActionGroup;
 import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.actions.ActionContext;
 import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonActionExtensionSite;
-import org.eclipse.ui.navigator.ICommonViewerSite;
 import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
 
 public class ProveedorDeAccionesObservatorio extends CommonActionProvider {
